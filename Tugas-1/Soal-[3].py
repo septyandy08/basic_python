9ac289a9e4785f33c9c916eb0afec2f632da7828
Muhammad Rizqy Septyandy <60462503+septyandy08@users.noreply.github.com>
2020-10-05 20:10:59 +0700

    Tugas 1

--- Tugas-1/Soal-[3].py
+++ Tugas-1/Soal-[3].py
@@ -1,11 +1,12 @@
 teori = float(input("Masukkan Nilai Ujian Teori Anda: "))
 praktek = float(input("Masukkan Nilai Ujian Praktek Anda: "))
+nilai_minimum = 70.0
 
-if teori >= 70.0 and praktek >= 70.0:
+if teori >= nilai_minimum and praktek >= nilai_minimum:
     print("Selamat, anda lulus!")
-elif teori >= 70.0 and praktek < 70.0:
+elif teori >= nilai_minimum and praktek < nilai_minimum:
     print("Anda harus mengulang ujian praktek.")
-elif teori < 70.0 and praktek >= 70.0:
+elif teori < nilai_minimum and praktek >= nilai_minimum:
     print("Anda harus mengulang ujian teori.")
 else:
     print("Anda harus mengulang ujian teori dan praktek.")
