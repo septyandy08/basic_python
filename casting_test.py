33ec88a093bceb68c4b110e9bfe57cd2285478d9
[septyandy08] <[m.rizqy@alumni.ui.ac.id]>
2020-10-25 21:33:37 +0700

    Pertemuan 2

---
+++ casting_test.py
@@ -0,0 +1,27 @@
+# Float to Integer
+x = 1.5
+print(x)
+print(type(x))
+
+y = int(x)
+print(y)
+print(type(y))
+
+# Integer to Float
+a = 100
+b = float(a)
+print(b)
+print(type(b))
+
+# String to Float
+x = "4.5"
+y = float(x)
+print(y)
+print(type(y))
+
+# Float to String
+a = 9.9
+b = str(a)
+print(b)
+print(type(b))
+
---
+++ datatypes_test.py
@@ -0,0 +1,15 @@
+string = "Ini ada string"
+print(string)
+print(type(string))
+
+integer = 100
+print(integer)
+print(type(integer))
+
+angka_decimal = 100.15
+print(angka_decimal)
+print(type(angka_decimal))
+
+boolean = True
+print(boolean)
+print(type(boolean))
